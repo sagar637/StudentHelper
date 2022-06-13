afdc0c25e00861bfdb41f46ed6bef9bb436b2096
Sagar <sagarsedhai5@gmail.com>
2022-06-13 19:39:46 +0545

    notificationwithdatedone

--- lib/Widgets/notificationcard.dart
+++ lib/Widgets/notificationcard.dart
@@ -1,39 +1,38 @@
 
-
-
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:studenthelper/Community/Feed/feedmain.dart';
-
-import '../Modelss/user.dart';
 import '../Resources/auth_methos.dart';
 import '../Utilis/utilis.dart';
 
 class NotificationCard extends StatefulWidget {
   final snap;
-   const NotificationCard({Key? key, this.snap}) : super(key: key);
+  final String uid;
+    NotificationCard({Key? key, this.snap, required this.uid}) : super(key: key);
   @override
   State<NotificationCard> createState() => _NotificationCardState();
 }
 class _NotificationCardState extends State<NotificationCard> {
   int commentLen = 0;
-  // bool isLikeAnimating = false;
   User? user;
-  //
+  var userData = {};
+  bool isLoading = false;
   @override
   void initState() {
     super.initState();
-    postUserDispay();
-    fetchCommentLen();
+    // postUserDispay();
+    DisplayCommentLen();
   }
-  postUserDispay() async {
-    User _user = await AuthMethods().getUserDetails();
-    setState(() {
-      user = _user;
-    });
-  }
-  fetchCommentLen() async {
+  // postUserDispay() async {
+  //   User _user = await AuthMethods().getUserDetails();
+  //   setState(() {
+  //     user = _user;
+  //   });
+  // }
+  DisplayCommentLen() async {
     try {
       QuerySnapshot snap = await FirebaseFirestore.instance
           .collection('posts')
@@ -49,28 +48,40 @@ class _NotificationCardState extends State<NotificationCard> {
     }
     setState(() {});
   }
+
   @override
   Widget build(BuildContext context) {
     return Card(
         child: ListTile(
-            title: Text(
-              widget.snap['username'] + 'Posted a Question',
-                 //' ${user!.username}'
+            title: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+               // userData['username'].toString()
+                widget.snap['username']
+                    + 'Posted a Question on',
+                  // ' ${user!.username}'
+              ),
             ),
-            subtitle: Text('${widget.snap['likes'].length} likes' '$commentLen comments'
+            subtitle: Text( DateFormat.yMMMd()
+                .format(widget.snap['datePublished'].toDate()),
+                //'${widget.snap['likes'].length} likes' '$commentLen comments'
             ),
             leading: CircleAvatar(
-                backgroundImage: NetworkImage(widget.snap['profImage'])),
+                backgroundImage: NetworkImage(
+                    widget.snap['profImage']
+                )),
             // trailing: Icon(Icons.arrow_forward),
             onTap: () {
               Navigator.of(context).push(
                   MaterialPageRoute(
                     builder: (context) => FeedScreen(
-                     // uid: (snapshot.data! as dynamic).docs[index]['uid'],
+                      // uid: (snapshot.data! as dynamic).docs[index]['uid'],
                     ),
                   ));
     })
+
     );
+
   }
 }
 
