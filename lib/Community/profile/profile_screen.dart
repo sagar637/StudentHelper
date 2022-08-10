f4fd7bf88b212c1fb126aec61f2a9025c4e0d7fd
Sagar <sagarsedhai5@gmail.com>
2022-08-10 16:10:34 +0545

    profilescreenuichanged

--- lib/Community/profile/profile_screen.dart
+++ lib/Community/profile/profile_screen.dart
@@ -98,7 +98,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
             }
           ),
         ],
-        centerTitle: false,
+        centerTitle: true,
       ),
       body: ListView(
         children: [
@@ -107,99 +107,100 @@ class _ProfileScreenState extends State<ProfileScreen> {
             child: Column(
               children: [
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     CircleAvatar(
                       backgroundColor: Colors.grey,
                       backgroundImage: NetworkImage(
                         userData['photoUrl'],
                       ),
-                      radius: 40,
+                      radius: 60,
                     ),
-                    Expanded(
-                      flex: 1,
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment:
-                            MainAxisAlignment.spaceEvenly,
-                            children: [
-                              buildStatColumn(postLen, "posts"),
-                              buildStatColumn(followers, "followers"),
-                              buildStatColumn(following, "following"),
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment:
-                            MainAxisAlignment.spaceEvenly,
-                            children: [
-                              FirebaseAuth.instance.currentUser!.uid ==
-                                  widget.uid
-                                  ? FollowButton(
-                                text: 'Your Profile',
-                                // backgroundColor:
-                                // mobileBackgroundColor,
-                                textColor: Colors.blue,
-                                borderColor: Colors.red,
-                                function: () async {
-                                  await AuthMethods().signOut();
-                                  Navigator.of(context)
-                                      .pushReplacement(
-                                    MaterialPageRoute(
-                                      builder: (context) =>
-                                         const LoginScreen( ),
-                                    ),
-                                  );
-                                },
-                              )
-                                  : isFollowing
-                                  ? FollowButton(
-                                text: 'Unfollow',
-                                //backgroundColor: Colors.white,
-                                textColor: Colors.black,
-                                borderColor: Colors.grey,
-                                function: () async {
-                                  await FireStoreMethods()
-                                      .followUser(
-                                    FirebaseAuth.instance
-                                        .currentUser!.uid,
-                                    userData['uid'],
-                                  );
-
-                                  setState(() {
-                                    isFollowing = false;
-                                    followers--;
-                                  });
-                                },
-                              )
-                                  : FollowButton(
-                                text: 'Follow',
-                                //backgroundColor: Colors.blue,
-                                textColor: Colors.white,
-                                borderColor: Colors.blue,
-                                function: () async {
-                                  await FireStoreMethods()
-                                      .followUser(
-                                    FirebaseAuth.instance
-                                        .currentUser!.uid,
-                                    userData['uid'],
-                                  );
-
-                                  setState(() {
-                                    isFollowing = true;
-                                    followers++;
-                                  });
-                                },
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
+                    // Expanded(
+                    //   flex: 1,
+                    //   child: Column(
+                    //     children: [
+                    //       Row(
+                    //         mainAxisSize: MainAxisSize.max,
+                    //         mainAxisAlignment:
+                    //         MainAxisAlignment.spaceEvenly,
+                    //         children: [
+                    //           buildStatColumn(postLen, "posts"),
+                    //           buildStatColumn(followers, "followers"),
+                    //           buildStatColumn(following, "following"),
+                    //         ],
+                    //       ),
+                    //       Row(
+                    //         mainAxisAlignment:
+                    //         MainAxisAlignment.spaceEvenly,
+                    //         children: [
+                    //           FirebaseAuth.instance.currentUser!.uid ==
+                    //               widget.uid
+                    //               ? FollowButton(
+                    //             text: 'Your Profile',
+                    //             // backgroundColor:
+                    //             // mobileBackgroundColor,
+                    //             textColor: Colors.blue,
+                    //             borderColor: Colors.red,
+                    //             function: () async {
+                    //               await AuthMethods().signOut();
+                    //               Navigator.of(context)
+                    //                   .pushReplacement(
+                    //                 MaterialPageRoute(
+                    //                   builder: (context) =>
+                    //                      const LoginScreen( ),
+                    //                 ),
+                    //               );
+                    //             },
+                    //           )
+                    //               : isFollowing
+                    //               ? FollowButton(
+                    //             text: 'Unfollow',
+                    //             //backgroundColor: Colors.white,
+                    //             textColor: Colors.black,
+                    //             borderColor: Colors.grey,
+                    //             function: () async {
+                    //               await FireStoreMethods()
+                    //                   .followUser(
+                    //                 FirebaseAuth.instance
+                    //                     .currentUser!.uid,
+                    //                 userData['uid'],
+                    //               );
+                    //
+                    //               setState(() {
+                    //                 isFollowing = false;
+                    //                 followers--;
+                    //               });
+                    //             },
+                    //           )
+                    //               : FollowButton(
+                    //             text: 'Follow',
+                    //             //backgroundColor: Colors.blue,
+                    //             textColor: Colors.white,
+                    //             borderColor: Colors.blue,
+                    //             function: () async {
+                    //               await FireStoreMethods()
+                    //                   .followUser(
+                    //                 FirebaseAuth.instance
+                    //                     .currentUser!.uid,
+                    //                 userData['uid'],
+                    //               );
+                    //
+                    //               setState(() {
+                    //                 isFollowing = true;
+                    //                 followers++;
+                    //               });
+                    //             },
+                    //           )
+                    //         ],
+                    //       ),
+                    //     ],
+                    //   ),
+                    // ),
                   ],
                 ),
                 Container(
-                  alignment: Alignment.centerLeft,
+                  alignment: Alignment.center,
                   padding: const EdgeInsets.only(
                     top: 15,
                   ),
@@ -207,22 +208,157 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     userData['username'],
                     style:  const TextStyle(
                       fontWeight: FontWeight.bold,
+                      fontSize: 25,
                     ),
                   ),
                 ),
-                Container(
-                  alignment: Alignment.centerLeft,
-                  padding: const EdgeInsets.only(
-                    top: 1,
+                Divider(
+                  color: Colors.black,
+                ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment:
+                    MainAxisAlignment.spaceEvenly,
+                    children: [
+                      buildStatColumn(postLen, "Questions"),
+                      buildStatColumn(postLen, "Likes"),
+                      buildStatColumn(followers, "followers"),
+                      buildStatColumn(following, "following"),
+                    ],
                   ),
-                  child: Text(
-                    userData['bio'],
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment:
+                      MainAxisAlignment.spaceEvenly,
+                      children: [
+                        FirebaseAuth.instance.currentUser!.uid ==
+                            widget.uid
+                            ? FollowButton(
+                          text: 'Your Profile',
+                          // backgroundColor:
+                          // mobileBackgroundColor,
+                          textColor: Colors.black,
+                          borderColor: Colors.black,
+                          function: () async {
+                            await AuthMethods().signOut();
+                            Navigator.of(context)
+                                .pushReplacement(
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                   const LoginScreen( ),
+                              ),
+                            );
+                          },
+                        )
+                            : isFollowing
+                            ? FollowButton(
+                          text: 'Unfollow',
+                          //backgroundColor: Colors.white,
+                          textColor: Colors.black,
+                          borderColor: Colors.grey,
+                          function: () async {
+                            await FireStoreMethods()
+                                .followUser(
+                              FirebaseAuth.instance
+                                  .currentUser!.uid,
+                              userData['uid'],
+                            );
+
+                            setState(() {
+                              isFollowing = false;
+                              followers--;
+                            });
+                          },
+                        )
+                            : FollowButton(
+                          text: 'Follow',
+                          //backgroundColor: Colors.blue,
+                          textColor: Colors.white,
+                          borderColor: Colors.blue,
+                          function: () async {
+                            await FireStoreMethods()
+                                .followUser(
+                              FirebaseAuth.instance
+                                  .currentUser!.uid,
+                              userData['uid'],
+                            );
+
+                            setState(() {
+                              isFollowing = true;
+                              followers++;
+                            });
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.only(
+                    top: 13,
+                  ),
+
+                  child: RichText(
+                              text: TextSpan(
+                                style: const TextStyle(color: Colors.black),
+                                children:  [
+                                   TextSpan(
+                                    text: 'About Me',
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+
+                  // child: Text(
+                  //   ' About Me:'
+                  //       ' ${ userData['bio']}',
+                  //   style: TextStyle(
+                  //     fontSize: 15,
+                  //   ),
+                  // ),
+                ),
+                Divider(
+                  indent: 100,
+                  endIndent: 100,
+                  thickness: 5,
+                  color: Colors.black,
+                ),
+             Padding(
+               padding: const EdgeInsets.all(12.0),
+               child: Text(
+                 ' ${ userData['bio']}',
+                  style: TextStyle(
+                    fontSize: 18,
                   ),
                 ),
+             ),
               ],
             ),
           ),
-          const Divider(),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text(
+              'Question Asked',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          const Divider(
+          ),
           FutureBuilder(
             future: FirebaseFirestore.instance
                 .collection('posts')
